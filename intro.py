b2cc3fbc8d3ee73f2e1e78e80ddcf816860f4bef
samadhi laksahan <samadhivkcom@gmail.com>
2024-12-05 16:55:05 +0530

    add test

--- intro.py
+++ intro.py
@@ -1,22 +1,52 @@
-# Namedtuple - When and why should you use namedtuples
+# Classes and Instances  
+
+# why we are use classes  
+# They allow us to logicallu group our variables and functions way that easy to reuse also easy to build upon it need be.  
+# method : function that assosiate with class.  
+
+# show instance variable contain data that unique to each intance  
+
+class Employee:
+    pass 
+
+emp_1 = Employee()
+emp_2 = Employee()
+
+print(emp_1) # <__main__.Employee object at 0x7fb0b4133380>
+print(emp_2) # <__main__.Employee object at 0x7fb0b37e4b90> 
+
+emp_1.first = 'Corey'
+emp_1.last = 'Schafer'
+emp_1.email = 'samadhivkcom@gmail.com'
+emp_1.pay = 50000
+
+emp_2.first = 'Test'
+emp_2.last = 'User'
+emp_2.email = 'test.user@gmail.com'
+emp_2.pay = 60000
+
+print(emp_1.email)
+print(emp_2.email)
+
+# input employee details when initialize the class and add method for printing full name of employee.     
+class Employee2:
+    def __init__(self, first, last, pay):
+        self.first = first 
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
 
 
-# show how to use tuple like dictionary but keeping data format  
+emp_1 = Employee2('Corey', 'Schafer', 50000)
+emp_2 = Employee2('Test', 'User', 60000)
 
-# normal way to define tuple  
-color = (55,155,255)
-print(color[1]) # by indexing not clear what is happening here  
+print(emp_1.first)
+print(emp_2.first)
+print(emp_1.fullname())
 
-# using dictionary   
-color = { 'red': 55, 'green':155, 'blue': 255}
-print(color)
-
-# using nametuple
-from collections import namedtuple 
-
-Color = namedtuple('Color', ['red', 'green', 'blue'])
-
-color = Color(55,155,255)
-
-print(color[1])
-print(color.green)
+# show another way to call "fullname" function.  
+print(Employee2.fullname(emp_1))
+ 
